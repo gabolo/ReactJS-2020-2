a9344b87f419403f5bd19ebad9d8b0efba5c1699
Simon Vecino <simonvecino@gmail.com>
2020-08-16 19:02:17 -0500

    Agregando Talleres 1 y 2

---
+++ Taller semana 2 - Sliding Puzzle/function.js
@@ -0,0 +1,52 @@
+function swapTiles(position1,position2) {
+  var temp = document.getElementById(position1).className;
+  document.getElementById(position1).className = document.getElementById(position2).className;
+  document.getElementById(position2).className = temp;
+}
+
+function mix() {
+for (var row=1;row<=3;row++) {
+   for (var column=1;column<=3;column++) {
+  
+    var row2=Math.floor(Math.random()*3 + 1); 
+    var column2=Math.floor(Math.random()*3 + 1); 
+    swapTiles("position"+row+column,"position"+row2+column2);
+  } 
+} 
+}
+
+function clickTile(row,column) {
+  var position = document.getElementById("position"+row+column);
+  var tile = position.className;
+  if (tile!="tile9") { 
+      
+       if (column<3) {
+         if ( document.getElementById("position"+row+(column+1)).className=="tile9") {
+           swapTiles("position"+row+column,"position"+row+(column+1));
+           return;
+         }
+       }
+      
+       if (column>1) {
+         if ( document.getElementById("position"+row+(column-1)).className=="tile9") {
+           swapTiles("position"+row+column,"position"+row+(column-1));
+           return;
+         }
+       }
+       
+       if (row>1) {
+         if ( document.getElementById("position"+(row-1)+column).className=="tile9") {
+           swapTiles("position"+row+column,"position"+(row-1)+column);
+           return;
+         }
+       }
+     
+       if (row<3) {
+         if ( document.getElementById("position"+(row+1)+column).className=="tile9") {
+           swapTiles("position"+row+column,"position"+(row+1)+column);
+           return;
+         }
+       } 
+  }
+  
+}
